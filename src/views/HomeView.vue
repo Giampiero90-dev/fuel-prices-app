6a6c6ec9af4208b3e11e9b962f71808b930982e4
Giampiero de Vita <giampiero.devita@hotmail.it>
2023-10-27 16:51:58 +0200

    First commit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,30 +1,29 @@
 <template>
   <main>
     <div>
-        <h2>Gas prices Overview</h2>
-        <router-link to="/create">+ Add a new tank station</router-link>
-        <ul>
-            <li v-for="(tankStation, index) in useTankStationStore.tankStations." :key="index">
-                <h3>{{ tankStation.title }}</h3>
-                <div>{{ tankStation.gasPrices }}</div>
-                <div>{{ tankStation.longitude }}</div>
-                <div>{{ tankStation.latitude }}</div>
-            </li>
-        </ul>
+      <h2>Tank stations and fuel prices</h2>
+      <router-link to="/">+ Add a new tank station</router-link>
+      <ul>
+        <li v-for="(tankStation, index) in useTankStationStore.tankStations" :key="index">
+          <h3>{{ tankStation.title }}</h3>
+          <div>{{ tankStation.gasPrices }}</div>
+          <div>{{ tankStation.longitude }}</div>
+          <div>{{ tankStation.latitude }}</div>
+        </li>
+      </ul>
     </div>
   </main>
 </template>
 
 <script>
-import TheWelcome from '../components/TheWelcome.vue'
 import { useTankStationStore } from '../stores/counter';
 
 export default {
   data() {
-  return {
-    useTankStationStore: useTankStationStore(),
+    return {
+      useTankStationStore: useTankStationStore(),
+    }
   }
-}
 }
 
 </script>
