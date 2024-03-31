ba0dc01b493594bf76cb8ea6ca9500b6bff170a4
Giampiero de Vita <giampiero.devita@hotmail.it>
2024-03-31 14:29:03 +0200

    Modified tankStation object structure

--- src/stores/tankStationStore.js
+++ src/stores/tankStationStore.js
@@ -5,74 +5,54 @@ export const useTankStationStore = defineStore('tankStationStore', {
     tankStations: [
       {
         name: "Q8",
-        fuelPrices: {
-          euro95: "2.05",
-          diesel: "1.8",
-          gas: "0.75",
-        },
-        address: {
-          street: "Kerkstraat",
-          unitNumber: "20",
-          zipCode: "1011BH",
-        },
+        euro95: "2.05",
+        diesel: "1.8",
+        gas: "0.75",
+        street: "Kerkstraat",
+        unitNumber: "20",
+        zipCode: "1011BH",
       },
       {
         name: "Esso",
-        fuelPrices: {
-          euro95: "2.05",
-          gas: "0.75",
-        },
-        address: {
-          street: "Linneusstraat",
-          unitNumber: "45",
-          zipCode: "1021RL",
-        },
+        euro95: "2.05",
+        gas: "0.75",
+        street: "Linneusstraat",
+        unitNumber: "45",
+        zipCode: "1021RL",
       }, {
         name: "Shell",
-        fuelPrices: {
-          euro95: "2.05",
-          diesel: "1.8",
-        },
-        address: {
-          street: "Koningsplein",
-          unitNumber: "67",
-          zipCode: "1034PL",
-        },
+        euro95: "2.05",
+        diesel: "1.8",
+        street: "Koningsplein",
+        unitNumber: "67",
+        zipCode: "1034PL",
       }, {
         name: "Makro",
-        fuelPrices: {
-          diesel: "1.8",
-          gas: "0.78",
-        },
-        address: {
-          street: "Waterlooplein",
-          unitNumber: "26",
-          zipCode: "1077EB",
-        },
+        diesel: "1.8",
+        gas: "0.78",
+        street: "Waterlooplein",
+        unitNumber: "26",
+        zipCode: "1077EB",
       },
       {
         name: "Texaco",
-        fuelPrices: {
-          euro98: "2.05",
-        },
-        address: {
-          street: "Eerstestraat",
-          unitNumber: "123",
-          zipCode: "1032FT",
-        },
+        euro98: "2.05",
+        street: "Eerstestraat",
+        unitNumber: "123",
+        zipCode: "1032FT",
       },
     ],
     fuelTypes: [
-      {id: "euro95", name: "Euro95"},
-      {id: "euro98", name: "Euro98"},
-      {id: "diesel", name: "Diesel"},
-      {id: "gas", name: "Gas"},
-  ],
+      { id: "euro95", name: "Euro95" },
+      { id: "euro98", name: "Euro98" },
+      { id: "diesel", name: "Diesel" },
+      { id: "gas", name: "Gas" },
+    ],
   }),
   getters: {
     filterByFuelType: (state) => {
       return (fuelType) => state.tankStations.filter(station => {
-        return station.fuelPrices[fuelType] !== undefined;
+        return station[fuelType] !== undefined;
       });
     }
   },
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,56 +1,57 @@
 <template>
-  <main>
-    <div>
-      <router-link to="/create">+ Add a new tank station</router-link>
-      <div>
-        <button @click="setFilter('all')">All</button>
-        <button v-for="(fuelType, index) in tankStationStore.fuelTypes" :key="index" 
-          @click="setFilter(fuelType.id)">{{fuelType.name}}</button>
-      </div>
-      <ul>
-        <li v-for="(tankStation, index) in selectedTankStations" :key="index">
-          <h3>Name: {{ tankStation.name }}</h3>
-          <div>
-            <h3>Fuel Prices:</h3>
-            <ul>
-              <template v-for="(fuelType, index) in tankStationStore.fuelTypes" :key="index">
-                <li v-if="tankStation.fuelPrices[fuelType.id] !== undefined">
-                  {{ fuelType.name }}: € {{ tankStation.fuelPrices[fuelType.id] }}
-                </li>
-              </template>
-            </ul>
-          </div>
-          <div>
-            <h3>Address:</h3>
-            <span>{{ tankStation.address.street }} {{ tankStation.address.unitNumber }} {{ tankStation.address.zipCode
-            }}</span>
-          </div>
-        </li>
-      </ul>
-    </div>
-  </main>
+	<main>
+		<div>
+			<router-link to="/create">+ Add a new tank station</router-link>
+			<div>
+				<button @click="setFilter('all')">All</button>
+				<button v-for="(fuelType, index) in tankStationStore.fuelTypes" :key="index"
+					@click="setFilter(fuelType.id)">{{ fuelType.name }}</button>
+			</div>
+			<ul>
+				<li v-for="(tankStation, index) in selectedTankStations" :key="index">
+					<h3>Name: {{ tankStation.name }}</h3>
+					<div>
+						<h3>Fuel Prices:</h3>
+						<ul>
+							<template v-for="(fuelType, index) in tankStationStore.fuelTypes" :key="index">
+								<li v-if="tankStation[fuelType.id] !== undefined">
+									{{ fuelType.name }}: € {{ tankStation[fuelType.id] }}
+								</li>
+							</template>
+						</ul>
+					</div>
+					<div>
+						<h3>Address:</h3>
+						<span>{{ tankStation.street }} {{ tankStation.unitNumber }} {{
+							tankStation.zipCode
+						}}</span>
+					</div>
+				</li>
+			</ul>
+		</div>
+	</main>
 </template>
 
 <script>
 import { useTankStationStore } from '../stores/tankStationStore';
 
 export default {
-  data() {
-    return {
-      tankStationStore: useTankStationStore(),
-      selectedFilter: "all",
-    }
-  },
-  methods: {
-    setFilter(fuelType) {
-      this.selectedFilter = fuelType;
-    },
-  },
-  computed: {
-    selectedTankStations: function () {
-      return this.selectedFilter === "all" ? this.tankStationStore.tankStations : this.tankStationStore.filterByFuelType(this.selectedFilter);
-    }
-  }
+	data() {
+		return {
+			tankStationStore: useTankStationStore(),
+			selectedFilter: "all",
+		}
+	},
+	methods: {
+		setFilter(fuelType) {
+			this.selectedFilter = fuelType;
+		},
+	},
+	computed: {
+		selectedTankStations: function () {
+			return this.selectedFilter === "all" ? this.tankStationStore.tankStations : this.tankStationStore.filterByFuelType(this.selectedFilter);
+		}
+	}
 }
 
 </script>
